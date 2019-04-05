c5537211a0a8921d8e8cb969c4e04fb63410d406
T-Cornelis <35939742+T-Cornelis@users.noreply.github.com>
2019-04-05 15:19:25 +0200

    Add files via upload

---
+++ Tom's Programs/huml.py
@@ -0,0 +1,187 @@
+
+import numpy as np
+import scipy as sp
+from IPython import embed
+from os import path
+import sys
+import time
+
+# To solve quadratic programming problems.
+from cvxopt import matrix, solvers
+solvers.options['show_progress'] = False
+
+# To compute gaussian kernel
+from scipy.spatial.distance import pdist, squareform
+
+# To assign numerical values to the labels.
+from sklearn import preprocessing
+
+class processing:
+	def __init__(self):
+		# Nothin yet
+		a = []
+
+	def slice(self, input_data, required_labels):
+
+		# We take the indeces which satisfy the required condition
+		tf_ind  = [ (_ in required_labels) for _ in input_data[:,-1]]
+		indeces = tuple(np.ix_(tf_ind)[0])
+
+		return input_data[indeces, :]
+
+def kernel_eval( data, kernel, **kwargs):
+
+	if kernel == 'linear':
+		return np.dot(data, data.T)
+	if kernel == 'exponential':
+		gamma_par = 1
+		return sp.exp(-squareform(pdist(data, 'sqeuclidean'))/ gamma_par**2)
+
+class svm:
+	def __init__(self, input_data, input_labels, **kwargs):
+
+		# Possible **kwargs:
+		# 1) lambda_par
+		# 2) kernel
+		
+		# Input dataset must be of the form:
+		# Rows    = datapoints
+		# Columns = features
+		self.feat_size  = input_data.shape[1]
+		self.data       = np.c_[input_data, input_labels]
+		self.labels     = input_labels
+
+		# We give numerical values to the labels
+		le = preprocessing.LabelEncoder()
+		le.fit(self.labels)
+		self.dict       = dict(zip(le.classes_, le.transform(le.classes_)))
+		self.dict_inv   = dict(zip(le.transform(le.classes_), le.classes_))
+		self.num_labels = len(le.classes_)
+		self.cur_label  = 0
+		self.label_vs   = 1
+		self.data[:,-1] = le.transform(input_labels)
+
+		# Here we will later just write one-vs-one
+		# data and labels.
+		self.data_ovo   = np.zeros(shape = 1)
+		self.labels_ovo = np.zeros(shape = 1)
+
+		# And the data without labels, with the associated size
+		self.data_red   = np.zeros(shape = 1)
+		self.data_size  = np.zeros(shape = 1)
+
+		# Now we introduce all the output variables.
+		# The vector of direction (depending on who we are confronting)
+		self.svm_ovo    = np.zeros(shape = (self.num_labels, self.num_labels, self.feat_size))
+		# The offset coefficient
+		self.svm_ovo_of = np.zeros(shape = (self.num_labels, self.num_labels, 1))
+
+		# We define the regularisation parameter:
+		if 'lambda_par' in kwargs:
+			self.lambda_par = kwargs['lambda_par']
+		else:
+			self.lambda_par = 0.3
+
+		# We define the kernel we want to use
+		if 'kernel' in kwargs:
+			self.kernel = kwargs['kernel']
+		else:
+			self.kernel = 'linear'
+
+	def prepare_data_ovo(self):
+
+		# We extract the data and labels which have to do with the 
+		# labels "cur_label" and "label_vs"
+		pr = processing()
+		self.data_ovo   = pr.slice(self.data, [self.cur_label, self.label_vs])
+
+		# We pick the last row of the reduced dataset
+		# And we reduce to two labels (+/-1).
+		self.labels_ovo = np.array([1.0 if x == self.cur_label else -1.0 for x in self.data_ovo[:,-1]])
+
+		# Finally we save the data without the labels, and it's dimension
+		self.data_red   = self.data_ovo[:,:-1]
+		self.data_size  = self.data_red.shape[0]
+
+	def fit_ovo(self):
+
+		# We follow the same definitions as in the Wikipedia page.
+		# We get the matrix for which we want to optimize
+		Q = np.multiply(kernel_eval(self.data_red, self.kernel), np.dot(self.labels_ovo, self.labels_ovo.T)).astype(np.double)
+		#vect     = (np.multiply(self.data_red.T, self.labels_ovo).T).astype(np.double)
+		Q_matrix = matrix(Q)
+
+		
+		# As well as the other parameters:
+		P_vector = matrix(-np.ones(shape = self.data_size))
+		Id       = np.eye(self.data_size)
+		G_matrix = matrix(np.block([[-Id], [Id]]))
+		H_vector = matrix(np.append(np.zeros(shape = self.data_size), \
+			np.ones(shape = self.data_size)/(self.data_size*self.lambda_par*2)) )
+		A_matrix = matrix(np.asmatrix(self.labels_ovo, dtype = np.float64))
+		B_vector = matrix([0.0])
+
+		# We solve the quadratic programming problem.
+		sol=solvers.qp(Q_matrix, P_vector, G_matrix, H_vector, A_matrix, B_vector)
+
+		# We save the optimal direction.
+		dual_direction  = np.asarray(list(sol['x']))
+
+		# Then we pass from the dual to the primal problem:
+		self.svm_ovo[self.cur_label, self.label_vs, :] = np.dot(dual_direction.T, vect)
+
+		# We still have to compute the offset (the affine translation b).
+		# This is not completely trivial. We have to use the complementary
+		# slackness condition from the Karush-Kuhn-Tucker conditions (see
+		# the associated Wikipedia page).
+
+		# First we take the data points which lie on the boundary:
+		active_points   = np.array([1.0 if np.abs(x)>1e-8 else 0.0 for x in dual_direction])
+
+		# Then for stability we take the average of the of the results
+		# in these directions (suggested by a MSE answer).
+		self.svm_ovo_of[self.cur_label, self.label_vs, :] = np.dot(active_points, np.dot(self.data_red, self.svm_ovo[self.cur_label,\
+		 self.label_vs, :]) - self.labels_ovo)/np.sum(active_points)
+
+	def fit(self):
+		#Here we fit the svm to the data
+
+		for label_index in range(self.num_labels):
+			for label_vs_index in range(label_index + 1, self.num_labels):
+
+				self.cur_label  = label_index
+				self.label_vs   = label_vs_index
+
+				# First we build the correct one-vs-all data
+				self.prepare_data_ovo()
+
+				# And we can find the svm on this label
+				self.fit_ovo()
+
+	def predict(self, input_dataset_test):
+
+		# We predict the one versus one classification first.
+		prediction_vector = np.zeros(shape = (input_dataset_test.shape[0], self.num_labels, self.num_labels))
+
+		for label_index in range(self.num_labels):
+			for label_vs_index in range(label_index+1, self.num_labels):
+				prediction_vector[:, label_index, label_vs_index] = np.dot(input_dataset_test, self.svm_ovo[label_index,\
+				 label_vs_index, :].T) - self.svm_ovo_of[label_index, label_vs_index, :].T
+
+				#We make the matrix antisymmetric.
+				prediction_vector[:, label_vs_index, label_index] = - prediction_vector[:, label_index, label_vs_index]
+
+		# We pass from numerical to +/-1 for voting:
+		prediction_vector= 0.5*(np.sign(prediction_vector) +1)
+		# Then sum all the results, to get the votes.
+		prediction_votes = np.zeros(shape = (input_dataset_test.shape[0], self.num_labels))
+		prediction_votes = np.sum(prediction_vector, axis = 2)
+
+		# And we choose the class that gets the most votes.
+		predictions_numerical = np.argmax(prediction_votes, axis =1)
+
+		# The prediction is a number. We transform it into a label
+		# via the preprocessing procedure.
+		predictions = [self.dict_inv[_] for _ in predictions_numerical]
+
+		return predictions
---
+++ Tom's Programs/iris_multiclass_svm_ovo.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import scipy as sp
+from IPython import embed
+from os import path
+import sys
+import time
+
+# To solve quadratic programming problems.
+from cvxopt import matrix, solvers
+
+# To write down results from classification.
+from sklearn.metrics import classification_report, confusion_matrix  
+
+# To split the data in train and test.
+from sklearn.model_selection import train_test_split 
+
+# To assign numerical values to the labels.
+from sklearn import preprocessing
+
+# SVM com Sklearn, for comparison.
+from sklearn.svm import SVC
+
+# Import hand-made SVM program.
+from huml import svm
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+# Assign colum names to the dataset
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+# Read dataset to pandas dataframe
+dataset = pd.read_csv(url, names=names) 
+
+# We take out the labels:
+data_points  = dataset.iloc[:, :-1].values
+data_label   = dataset.iloc[:, 4].values
+
+# We split the data. 
+data_test, data_train, label_test, label_train = train_test_split(data_points, data_label, test_size=0.30)
+
+# We creat the SVM class and fit it to the data
+exmpl = svm(data_train, label_train, lambda_par = 0.2, kernel = 'exponential')
+embed()
+exmpl.fit()
+
+# Now we can do predictions
+predict_test = exmpl.predict(data_test)
+
+# We see how good we perform.
+print(confusion_matrix(label_test, predict_test))  
+print(classification_report(label_test, predict_test))
+
+# We can also plot the results:
+color_dict = {0: 'tab:blue', 1: 'tab:orange', 2: 'tab:green'}
+
+le = preprocessing.LabelEncoder()
+le.fit(data_label)
+
+numerical_label = le.transform(data_label)
+my_cmap = [color_dict[numerical_label[_]] for _ in range(len(data_label))]
+
+# To plot it we have to projsect on a two-dimensional space
+# The first coordinate will be the projection along the svm_direction
+# centered around the offset.\
+first_direction    = np.zeros(shape = 4)
+first_direction[0] = 1.0
+plt.scatter(np.dot(data_points, exmpl.svm_ovo[0,1,:].T)- exmpl.svm_ovo_of[0,1,:],\
+ np.dot(data_points, first_direction.T), c = my_cmap)
+plt.show()
+plt.scatter(np.dot(data_points, exmpl.svm_ovo[1,2,:].T)- exmpl.svm_ovo_of[1,2,:],\
+ np.dot(data_points, first_direction.T), c = my_cmap)
+plt.show()
+plt.scatter(np.dot(data_points, exmpl.svm_ovo[0,2,:].T)- exmpl.svm_ovo_of[0,2,:],\
+ np.dot(data_points, first_direction.T), c = my_cmap)
+plt.show()
+
+# We can also confron this with the sklearn SVM:
+clf = SVC( kernel = 'linear')
+clf.fit(data_train, label_train)
+
+print(confusion_matrix(label_test, clf.predict(data_test)))  
+print(classification_report(label_test, clf.predict(data_test)))
+
+# Let us test how we perform on one the last two flowers.
+
+from huml import processing
+from sklearn.svm import LinearSVC
+
+embed()
+
+pr = processing()
+dataset_v = dataset.values
+data_sliced = pr.slice(dataset_v, ['Iris-versicolor', 'Iris-virginica'])
+
+# We take out the labels:
+data_points  = data_sliced[:, :-1]
+data_label   = data_sliced[:, 4]
+
+# We split the data. 
+data_test, data_train, label_test, label_train = train_test_split(data_points, data_label, test_size=0.30)
+
+for _ in range(1, 21):
+
+	# We creat the SVM class and fit it to the data
+	exmpl = svm(data_train, label_train, _/(25.0))
+	exmpl.fit()
+
+	# Now we can do predictions
+	predict_test = exmpl.predict(data_test)
+
+	print('lambda = {}'.format(_/(25.0)))
+	# We see how good we perform.
+	print(confusion_matrix(label_test, predict_test))  
+	print(classification_report(label_test, predict_test))
+
+	# We confront with Sklearn
+	clf = LinearSVC()
+	clf.fit(data_train, label_train)
+
+	print(confusion_matrix(label_test, clf.predict(data_test)))  
+	print(classification_report(label_test, clf.predict(data_test)))
